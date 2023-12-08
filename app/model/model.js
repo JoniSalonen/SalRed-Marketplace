0b6e180b5bb6467c20de063c4a292bf4d42b615d
JoniSalonen <101516095+JoniSalonen@users.noreply.github.com>
2023-12-08 17:39:49 +0200

    Last commit

--- app/model/model.js
+++ app/model/model.js
@@ -4,9 +4,9 @@ require('dotenv').config();
 
 var connection = mysql.createConnection(
     {
-    host: process.env.DB_HOST_1, 
-    user: process.env.DB_USER_1, 
-    password: process.env.DB_PASSWORD_1, 
+    host: process.env.DB_HOST, 
+    user: process.env.DB_USER, 
+    password: process.env.DB_PASSWORD, 
     database: process.env.DB_NAME, 
     port: process.env.DB_PORT, 
     //ssl:{ca:fs.readFileSync("{ca-cert filename}")}
